6aa1a622bc1feb7a9c161ec831619b00b905b2bf
Jesper Persson <jesper.92.persson@gmail.com>
2021-01-31 14:33:57 +0100

    Remove dead code

--- MineSweeperConsole/MineSweeperConsole.cpp
+++ MineSweeperConsole/MineSweeperConsole.cpp
@@ -7,32 +7,9 @@
 
 using namespace std;
 
-struct Blank{};
-struct Unknown{};
-struct Mine{};
-struct GuessMine{};
-struct GuessNotMine{};
-struct Number { int num; };
-using TileType = std::variant<Blank, Unknown, Mine, GuessMine, GuessNotMine, Number>;
-
-
 int main()
 {
-    int size = 9;
-    vector<vector<TileType>> board;
-
-    for (int row = 0; row < size; row++)
-    {
-        vector<TileType> boardRow;
-        for (int column = 0; column < size; column++)
-        {
-            boardRow.push_back(Unknown{});
-        }
-        board.push_back(boardRow);
-    }
-
     std::cout << "Hello World!\n";
-
 }
 
 
